605ab2dccb14f6416cd90ef85b230087734e4c2b
VirtualBox <VirtualBox@server.local>
2019-11-15 13:51:24 +0800

    Updates

---
+++ precache-manifest.ba57cb41674cbf115ded6f41bdf36954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eeeed9beaff49450b38a02c361cbb4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "93770cbd0e520d299fb1",
+    "url": "/monsters-rolodex/static/css/main.c060b6ed.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "93770cbd0e520d299fb1",
+    "url": "/monsters-rolodex/static/js/main.7581503f.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
